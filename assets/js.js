3db4cc5671dff326c446bf6f562d9f0ac4a939e9
KCaseyChamberlain <caseygchamberlain@gmail.com>
2022-01-22 16:21:51 -0700

    color cordinate events/hours

--- assets/js.js
+++ assets/js.js
@@ -7,6 +7,41 @@ var text = $(this)
 
 var textInput = $("<textarea>")
     .addClass("col-9 event-box row")
-    .val(text);
     $(this).replaceWith(textInput);
+    textInput.trigger("focus");
 });
+
+// Function finds current time and displays it in the header
+var currentTime = function () {
+    timeEL = document.querySelector('#currentDay')
+    timeEL.innerHTML = moment().format('MMMM Do YYYY, hh:mm');
+    // console.log(timeEL)
+}
+setInterval(currentTime, 1000);
+
+
+// Function compares the current hour to the event's set hour-num and applies 1/3 styles accordingly
+var hoursUpdater = function () {
+    var currentHour = moment().hours();
+    $('.event-box').each(function () {
+        var hourNum = parseInt($(this).attr("hour-num"))
+
+        if (hourNum < currentHour) {
+            $(this).addClass("past");
+        }
+
+        else if (hourNum > currentHour) {
+            $(this).addClass("future");
+        }
+
+        else {
+            $(this).addClass("present");
+        }
+
+    })
+}
+setInterval(hoursUpdater, 100)
+
+
+
+
